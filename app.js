28c182f1ef18f2d789248ce3877165b301b5570a
irfan maulanasam <alternatiflab@gmail.com>
2020-12-21 19:33:20 +0700

    setting up react environment

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const app = () => {
+  return (
+    <View>
+      <Text>yuk ahh</Text>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({})
